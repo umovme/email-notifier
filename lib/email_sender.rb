be67ef92e370e3cc8f730ee8c18a229a03256713
joleques <joleques@gmail.com>
2015-10-01 17:05:05 -0300

    teste do load options ajustado

--- lib/email_sender.rb
+++ lib/email_sender.rb
@@ -9,24 +9,27 @@ class EmailSender
 		YAML.load_file(File.join(File.dirname(__FILE__), '../conf/email_options.yml'))
 	end
 	
-	def get_settings _to
+	def get_settings 
 		options = load_options
-		Pony.options = {:via => :smtp, :to => _to, :via_options => {
-			:address => options['address'],
-			:port => options['port'],
-			:enable_starttls_auto => options['enable_starttls_auto'],
-			:user_name => options['user_name'],
-			:password => options['password'],
+		Pony.options = {:via => :smtp, :via_options => {
+			:address => options['config']['address'],
+			:port => options['config']['port'],
+			:enable_starttls_auto => options['config']['enable_starttls_auto'],
+			:user_name => options['config']['user_name'],
+			:password => options['config']['password'],
 			:authentication => :plain,
-			:domain => options['domain']
+			:domain => options['config']['domain']
 		}}
 	end
 
-	def send to, subject, body
-		get_settings(to)
+	def send email_rule
+		get_settings
 		begin
-		 Pony.mail(:subject => subject ,:body => body)
-		 puts 'Email enviado com sucesso!'
+		 Pony.mail(:to => email_rule.to_index,
+		 	:subject => email_rule.subject_index ,
+		 	:body => email_rule.body_index,
+		 	:cc =>email_rule.to_index)
+		 puts email_rule.to_index + 'Email enviado com sucesso!'
 		rescue Exception=>e
 			puts e
 		end
--- spec/email_sender_spec.rb
+++ spec/email_sender_spec.rb
@@ -7,8 +7,12 @@ RSpec.describe EmailSender, "#Send Email" do
 		it "load email options" do
 			email_sender = EmailSender.new
 			options = email_sender.load_options
-			#options["config"].index["address"].exists
-			expect(options["config"]["address"]).to exist
+			expect(options["config"].keys.include?('address')).to be true
+			expect(options["config"].keys.include?('port')).to be true
+			expect(options["config"].keys.include?('enable_starttls_auto')).to be true
+			expect(options["config"].keys.include?('user_name')).to be true
+			expect(options["config"].keys.include?('password')).to be true
+			expect(options["config"].keys.include?('domain')).to be true
 			expect(options.length).to eq(1)
 		end
 
