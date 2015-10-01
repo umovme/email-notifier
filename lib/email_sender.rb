b4c9b926f971bdc065d408fbf0950c7ec71c2161
joleques <joleques@gmail.com>
2015-10-01 17:38:05 -0300

    criando o teste para validar o envio de email

--- lib/email_sender.rb
+++ lib/email_sender.rb
@@ -5,13 +5,17 @@ require 'yaml'
 class EmailSender
 
 
+	def initialize(pony)
+		@pony = Pony
+	end
+
 	def load_options
 		YAML.load_file(File.join(File.dirname(__FILE__), '../conf/email_options.yml'))
 	end
 	
 	def get_settings 
 		options = load_options
-		Pony.options = {:via => :smtp, :via_options => {
+		@pony.options = {:via => :smtp, :via_options => {
 			:address => options['config']['address'],
 			:port => options['config']['port'],
 			:enable_starttls_auto => options['config']['enable_starttls_auto'],
@@ -25,7 +29,7 @@ class EmailSender
 	def send email_rule
 		get_settings
 		begin
-		 Pony.mail(:to => email_rule.to_index,
+		 @pony.mail(:to => email_rule.to_index,
 		 	:subject => email_rule.subject_index ,
 		 	:body => email_rule.body_index,
 		 	:cc =>email_rule.to_index)
--- spec/email_sender_spec.rb
+++ spec/email_sender_spec.rb
@@ -1,12 +1,17 @@
+require 'rubygems'
 require 'email_sender'
 
 RSpec.describe EmailSender, "#Send Email" do
 	
-	context "when I get to send email_roule" do
+	describe "when I get to send email_roule" do
+
+		before :all do
+			@pony = double('pony')
+			@email_sender = EmailSender.new @pony
+		end
 		
 		it "load email options" do
-			email_sender = EmailSender.new
-			options = email_sender.load_options
+			options = @email_sender.load_options
 			expect(options["config"].keys.include?('address')).to be true
 			expect(options["config"].keys.include?('port')).to be true
 			expect(options["config"].keys.include?('enable_starttls_auto')).to be true
@@ -14,6 +19,10 @@ RSpec.describe EmailSender, "#Send Email" do
 			expect(options["config"].keys.include?('password')).to be true
 			expect(options["config"].keys.include?('domain')).to be true
 			expect(options.length).to eq(1)
+		end
+
+		it "Should raise an error when send email" do
+
 		end
 
 	end
